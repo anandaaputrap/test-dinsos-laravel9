3055a0eeefb2edcb0e05e680a61c4ef67621305d
anandaaputrap <anandapratama4869@gmail.com>
2023-01-02 06:29:10 +0700

    da

--- resources/views/pendapatan/home.blade.php
+++ resources/views/pendapatan/home.blade.php
@@ -6,15 +6,8 @@
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Pendapatan</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Pendapatan</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
+          <!-- /.col -->
+          <!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
